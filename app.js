48a7076a9d0ad252a141af802f65dc0f5f0e0e4b
LuckySans <ahmadrizky586@gmail.com>
2022-12-05 22:37:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 require('dotenv/config')
 
+const cors = require("cors")
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({
     extended: true
